6ee780e841d1323d3ddf0121262b2381673bc273
unknown <oi.malyarenko@gmail.com>
2022-01-24 16:33:52 +0200

    Updates

---
+++ precache-manifest.7ddbaa88c9188650c1231d4d2aba2d73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "606d722d364d5650cc8448175459030e",
+    "url": "/pwz-app/index.html"
+  },
+  {
+    "revision": "16ef7b1ef499549ffdf1",
+    "url": "/pwz-app/static/css/main.a3459d3b.chunk.css"
+  },
+  {
+    "revision": "64154221ce7bafc6c0b9",
+    "url": "/pwz-app/static/js/2.0d15f8e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pwz-app/static/js/2.0d15f8e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16ef7b1ef499549ffdf1",
+    "url": "/pwz-app/static/js/main.7c928cf6.chunk.js"
+  },
+  {
+    "revision": "7ff1da00d17a77c5ad4c",
+    "url": "/pwz-app/static/js/runtime-main.3ba624b3.js"
+  },
+  {
+    "revision": "b9e65c667436d5b68b4d286eebd07861",
+    "url": "/pwz-app/static/media/3.b9e65c66.jpg"
+  }
+]);
